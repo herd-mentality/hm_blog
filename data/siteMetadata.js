064e484c7741ad449996c06a0685d87aa54c4a25
Darren Wong <email@darrenwong.net>
2023-02-28 22:05:54 +1100

    Further updates to favicon and metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,22 +3,22 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
   title: 'The Herd Mentality',
-  author: 'Tails Azimuth',
+  author: '',
   headerTitle: 'The Herd Mentality',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
-  theme: 'system', // system, dark or light
+  theme: 'dark', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
+  email: 'hello@herdmentality.xyz',
   github: 'https://github.com/herd-mentality/',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  twitter: '',
+  facebook: '',
+  youtube: '',
+  linkedin: '',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
