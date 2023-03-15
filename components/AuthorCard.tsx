2c3e0dcd211133b63c0604651e588340941b573d
Darren Wong <email@darrenwong.net>
2023-03-16 00:12:04 +1100

    MVP Authors page

---
+++ components/AuthorCard.tsx
@@ -0,0 +1,70 @@
+import Image from './Image'
+import Link from './Link'
+import SocialIcon from '@/components/social-icons'
+// TODO:
+const AuthorCard = ({ name, description, avatar, href, email, github, linkedin, twitter }) => (
+  <div className="md max-w-[544px] p-4 md:w-1/2">
+    <div
+      className={`${
+        avatar && 'h-full'
+      }  overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
+    >
+      <div className="mt-6 ml-6 flex">
+        <div className="flex flex-col justify-center">
+          {avatar &&
+            (href ? (
+              <Link href={href} aria-label={`See ${name}'s posts`}>
+                <Image
+                  alt={name}
+                  src={avatar}
+                  className="h-20 w-20 rounded-full"
+                  width={544}
+                  height={306}
+                />
+              </Link>
+            ) : (
+              <Image
+                alt={name}
+                src={avatar}
+                className="h-20 w-20 rounded-full"
+                width={544}
+                height={306}
+              />
+            ))}
+        </div>
+        <div className="ml-4 flex flex-col justify-center">
+          <Link href={href} aria-label={`See ${name}'s posts`}>
+            <h2 className="nav-link mb-0 text-2xl !font-bold leading-8 tracking-tight">{name}</h2>
+          </Link>
+          <h3 className="prose text-gray-500 dark:text-gray-400">Role title</h3>
+          <div className="flex space-x-3 py-1">
+            {email && <SocialIcon kind="mail" href={`mailto:${email}`} size={6} />}
+            {github && <SocialIcon kind="github" href={github} size={6} />}
+            {linkedin && <SocialIcon kind="linkedin" href={linkedin} size={6} />}
+            {twitter && <SocialIcon kind="twitter" href={twitter} size={6} />}
+          </div>
+        </div>
+      </div>
+      {/* <div className="ml-6 flex space-x-3 pt-6">
+        {email && <SocialIcon kind="mail" href={`mailto:${email}`} />}
+        {github && <SocialIcon kind="github" href={github} />}
+        {linkedin && <SocialIcon kind="linkedin" href={linkedin} />}
+        {twitter && <SocialIcon kind="twitter" href={twitter} />}
+      </div> */}
+      <div className="p-6">
+        <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">{description}</p>
+        {href && (
+          <Link
+            href={href}
+            className="tag-link text-base font-medium leading-6"
+            aria-label={`See ${name}'s posts`}
+          >
+            See {name.match(/^\w+/g)[0]}'s posts &rarr;
+          </Link>
+        )}
+      </div>
+    </div>
+  </div>
+)
+
+export default AuthorCard
---
+++ pages/authors.tsx
@@ -0,0 +1,37 @@
+import siteMetadata from '@/data/siteMetadata'
+import { allAuthors } from 'contentlayer/generated'
+import AuthorCard from '@/components/AuthorCard'
+import { PageSEO } from '@/components/SEO'
+
+export default function Authors() {
+  return (
+    <>
+      <PageSEO title={`Authors - ${siteMetadata.author}`} description={siteMetadata.description} />
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Authors
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">Meet the Herd</p>
+        </div>
+        <div className="container py-12">
+          <div className="-m-4 flex flex-wrap">
+            {allAuthors.map((a) => (
+              <AuthorCard
+                key={a.name}
+                name={a.name}
+                description={a.body.raw}
+                avatar={a.avatar}
+                href={'/'}
+                email={a.email}
+                github={a.github}
+                linkedin={a.linkedin}
+                twitter={a.twitter}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
