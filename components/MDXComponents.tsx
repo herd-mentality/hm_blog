804362286c3e8402fa1183745c6f6a0de915432a
Darren Wong <email@darrenwong.net>
2023-04-30 18:35:26 +1000

    Added slug MVPs, headings now link to themselves

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -4,6 +4,8 @@ import { MDXLayout, ComponentMap } from 'pliny/mdx-components'
 import { TOCInline } from 'pliny/ui/TOCInline'
 import { Pre } from 'pliny/ui/Pre'
 import { BlogNewsletterForm } from 'pliny/ui/NewsletterForm'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 import Image from './Image'
 import CustomLink from './Link'
@@ -13,12 +15,31 @@ export const Wrapper = ({ layout, content, ...rest }: MDXLayout) => {
   return <Layout content={content} {...rest} />
 }
 
-// Add custom headings here
+interface AutoLinkedHeadingProps {
+  tag: keyof JSX.IntrinsicElements
+  id: string
+}
+
+const AutoLinkedHeading: React.FC<AutoLinkedHeadingProps> = ({ tag: Tag, id, ...props }) => {
+  const router = useRouter()
+
+  return (
+    <Link href={`${router.asPath}#${id}`} passHref>
+      <Tag id={id} {...props} />
+    </Link>
+  )
+}
 
 export const MDXComponents: ComponentMap = {
   Image,
   TOCInline,
   a: CustomLink,
+  h1: (props) => <AutoLinkedHeading tag="h1" {...props} />,
+  h2: (props) => <AutoLinkedHeading tag="h2" {...props} />,
+  h3: (props) => <AutoLinkedHeading tag="h3" {...props} />,
+  h4: (props) => <AutoLinkedHeading tag="h4" {...props} />,
+  h5: (props) => <AutoLinkedHeading tag="h5" {...props} />,
+  h6: (props) => <AutoLinkedHeading tag="h6" {...props} />,
   pre: Pre,
   wrapper: Wrapper,
   BlogNewsletterForm,
