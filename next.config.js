aec926ff11e51ebf347daf00fc5a97543556764c
Darren Wong <email@darrenwong.net>
2023-05-08 17:52:44 +1000

    Added Google Analytics

--- next.config.js
+++ next.config.js
@@ -7,11 +7,11 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app https://www.google-analytics.com,;
   style-src 'self' 'unsafe-inline' giscus.app;
-  img-src * blob: data:;
+  img-src * https://www.google-analytics.com www.google-analytics.com https://stats.g.doubleclick.net blob: data:;
   media-src 'none';
-  connect-src *;
+  connect-src * https://www.google-analytics.com www.google-analytics.com https://stats.g.doubleclick.net;
   font-src 'self';
   frame-src giscus.app
 `
