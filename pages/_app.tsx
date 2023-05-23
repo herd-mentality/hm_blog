6e6b81c67ad1c81226caec0cf4ea06419587bcdf
Darren Wong <email@darrenwong.net>
2023-05-23 21:15:44 +1000

    Replace template's analytics handler with google analytics manual setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,9 +9,10 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
 import siteMetadata from '@/data/siteMetadata'
-import { Analytics } from 'pliny/analytics'
+// import { Analytics } from 'pliny/analytics'
 import { SearchProvider } from 'pliny/search'
 import LayoutWrapper from '@/components/LayoutWrapper'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -19,7 +20,28 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
-      <Analytics analyticsConfig={siteMetadata.analytics} />
+      <Script
+        strategy="afterInteractive"
+        src={'https://www.googletagmanager.com/gtag/js?id=' + process.env.GOOGLE_ANALYTICS_ID}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html:
+            `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '` +
+            process.env.GOOGLE_ANALYTICS_ID +
+            `', {
+            page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      {/* <Analytics analyticsConfig={siteMetadata.analytics} /> */}
       <LayoutWrapper>
         <SearchProvider searchConfig={siteMetadata.search}>
           <Component {...pageProps} />
