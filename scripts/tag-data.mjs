4a9efc8cfdcf3e8db72e9e905a9cadd5a25c2caa
Darren Wong <me@darrenwong.id.au>
2024-01-28 00:25:29 +1100

    ENH Updated post-build actions to include a tag counter

---
+++ scripts/tag-data.mjs
@@ -0,0 +1,23 @@
+import { writeFileSync } from 'fs'
+import GithubSlugger from 'github-slugger'
+import { allBlogs } from '../.contentlayer/generated/index.mjs'
+
+function createTagCount() {
+  const tagCount = {}
+  allBlogs.forEach((file) => {
+    if (file.tags && file.draft !== true) {
+      file.tags.forEach((tag) => {
+        const formattedTag = GithubSlugger.slug(tag)
+        if (formattedTag in tagCount) {
+          tagCount[formattedTag] += 1
+        } else {
+          tagCount[formattedTag] = 1
+        }
+      })
+    }
+  })
+  writeFileSync('public/tag-data.json', JSON.stringify(tagCount))
+  console.log('Tag counts generated...')
+}
+
+export default createTagCount
