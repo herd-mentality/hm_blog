e8c06a4b4d0e14ad5be32b63005858a5089d67bc
wongd-hub <me@darrenwong.id.au>
2025-08-10 15:00:34 +1000

    fix: some issues with post-build after upgrading to node 22

--- scripts/search.mjs
+++ scripts/search.mjs
@@ -1,9 +1,16 @@
 import { writeFileSync } from 'fs'
 import { allCoreContent } from 'pliny/utils/contentlayer.js'
-import { allBlogs } from '../.contentlayer/generated/index.mjs'
 import siteMetadata from '../data/siteMetadata.js'
+import { readFileSync } from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 
 const search = () => {
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = dirname(__filename)
+  const allBlogs = JSON.parse(
+    readFileSync(resolve(__dirname, '../.contentlayer/generated/Blog/_index.json'), 'utf8')
+  )
   if (siteMetadata?.search?.kbarConfig?.searchDocumentsPath) {
     writeFileSync(
       `public/${siteMetadata.search.kbarConfig.searchDocumentsPath}`,
--- scripts/sitemap.mjs
+++ scripts/sitemap.mjs
@@ -1,8 +1,15 @@
 import { generateSitemap } from 'pliny/utils/generate-sitemap.js'
 import siteMetadata from '../data/siteMetadata.js'
-import { allBlogs } from '../.contentlayer/generated/index.mjs'
+import { readFileSync } from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 
 const sitemap = () => {
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = dirname(__filename)
+  const allBlogs = JSON.parse(
+    readFileSync(resolve(__dirname, '../.contentlayer/generated/Blog/_index.json'), 'utf8')
+  )
   generateSitemap(siteMetadata.siteUrl, allBlogs)
   console.log('Sitemap generated...')
 }
